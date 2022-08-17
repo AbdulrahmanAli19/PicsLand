0488e5b5931c5039ab94a98c0bd0dc5a9fd81dfd
AbdulrahmanAli19 <Abdulrahman.ali19@outlook.com>
2022-08-17 23:41:26 +0200

    adding LoadStateAdapter to resume the paging process if connections is lost or any error has been accrued, adding layout for error, adding lottie library to load json animation

---
+++ app/src/main/java/abdulrahman/ali19/intrazero/presentation/ui/home/adapters/PageLoadStateAdapter.kt
@@ -0,0 +1,21 @@
+package abdulrahman.ali19.intrazero.presentation.ui.home.adapters
+
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+
+class PageLoadStateAdapter(
+    private val retry: () -> Unit
+) : LoadStateAdapter<PagesLoadStateViewHolder>() {
+
+    override fun onBindViewHolder(holder: PagesLoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ): PagesLoadStateViewHolder {
+        return PagesLoadStateViewHolder.create(parent, retry)
+    }
+}
---
+++ app/src/main/java/abdulrahman/ali19/intrazero/presentation/ui/home/adapters/PagesLoadStateViewHolder.kt
@@ -0,0 +1,38 @@
+package abdulrahman.ali19.intrazero.presentation.ui.home.adapters
+
+import abdulrahman.ali19.intrazero.R
+import abdulrahman.ali19.intrazero.databinding.ItemLoadStateBinding
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+
+class PagesLoadStateViewHolder(
+    private val binding: ItemLoadStateBinding,
+    retry: () -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.retryBtn.setOnClickListener { retry.invoke() }
+    }
+
+    fun bind(loadState: LoadState) {
+        if (loadState is LoadState.Error)
+            binding.errorMsg.text = loadState.error.localizedMessage
+
+        binding.progressBar.isVisible = loadState is LoadState.Loading
+        binding.retryBtn.isVisible = loadState !is LoadState.Loading
+        binding.errorMsg.isVisible = loadState !is LoadState.Loading
+
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, retry: () -> Unit): PagesLoadStateViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_load_state, parent, false)
+            val binding = ItemLoadStateBinding.bind(view)
+            return PagesLoadStateViewHolder(binding, retry)
+        }
+    }
+}

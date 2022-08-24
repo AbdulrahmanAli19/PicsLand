c6e08b8106fe5c360a919260ba5fbb091490a28f
AbdulrahmanAli19 <Abdulrahman.ali19@outlook.com>
2022-08-24 02:49:42 +0200

    Ads is visible once it gets loaded

--- app/src/main/java/abdulrahman/ali19/intrazero/presentation/ui/home/adapters/AdViewHolder.kt
+++ app/src/main/java/abdulrahman/ali19/intrazero/presentation/ui/home/adapters/AdViewHolder.kt
@@ -3,7 +3,9 @@ package abdulrahman.ali19.intrazero.presentation.ui.home.adapters
 import abdulrahman.ali19.intrazero.databinding.AdLayoutBinding
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 
 class AdViewHolder(binding: AdLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -14,6 +16,12 @@ class AdViewHolder(binding: AdLayoutBinding) : RecyclerView.ViewHolder(binding.r
                 LayoutInflater.from(view.context), view, false
             )
             binding.adView.loadAd(AdRequest.Builder().build())
+            binding.adView.adListener = object: AdListener(){
+                override fun onAdLoaded() {
+                    binding.root.isVisible = true
+                    binding.adView.isVisible = true
+                }
+            }
             return AdViewHolder(binding)
         }
     }
